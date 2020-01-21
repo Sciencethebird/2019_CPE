8a5f0581848b680e72bbed03f37fcb0584a58132
sciencethebird <sciencethebird@gmail.com>
2020-01-21 17:08:54 +0800

    Update 22801_Doom's_Day_Algorithm.cpp

--- UVA_1_star/22801_Doom's_Day_Algorithm.cpp
+++ UVA_1_star/22801_Doom's_Day_Algorithm.cpp
@@ -1,16 +1,27 @@
 //  22801:Doom's Day Algorithm
 
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    long long a, b, diff;
-      while(cin >> a >> b){
-          diff = a-b;
-          if(diff >= 0 ) cout << diff<< endl;
-          else cout << -diff << endl;
-      }
-      return 0;
+    int cases;
+    cin >> cases;
+    int md[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    string dn[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    for(int i = 0; i < cases ; i++){
+        int m , d;
+        cin >> m >> d;
+        int days = d;
+        for(int j = 0; j< m-1; j++)
+            days += md[j];
+        
+        //cout << days << endl;
+        days %= 7;
+        days += 5;
+        days %= 7;
+        cout << dn[days] << endl;
+    }
+    return 0;
     
 }
