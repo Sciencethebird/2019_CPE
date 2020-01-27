e4d529a025869b9f92af5955554c1e13db6666a0
sciencethebird <sciencethebird@gmail.com>
2020-01-27 14:35:52 +0800

    Create 10411_Back_to_High_School_Physics.cpp

---
+++ UVA_1_star/10411_Back_to_High_School_Physics.cpp
@@ -0,0 +1,16 @@
+//  10411:Back to High School Physics
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int v, t;
+    while(cin >> v >> t){
+        cout << 2*v*t << endl;
+    }
+    return 0;
+}
